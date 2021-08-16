918cc0d19a78ac99b0cf4dcad0035efc18863ddb
Nik Fernandez <niklas_fernandez@comcast.net>
2021-08-16 14:25:54 -0500

    Refactor ALL file operations to a separate file

--- code_timer.py
+++ code_timer.py
@@ -1,38 +1,10 @@
 import time
 import datetime
-import os
-
-# Display how much studying has been completed today
-def print_todays_practice_time():
-    FILENAME = 'timer_data.txt'
-    todays_time_studying = 0
-    
-    # read data from file -- if it doesn't exist then create it'
-    with open(f'{FILENAME}', 'a+') as f:
-    # parse through the data and read in the records only for today
-        lines = f.readlines()
-    # go through each line, split it into an array, and look for today's date
-        for line in lines:
-            record = line.split(',')
-        # remove the newline character from the time
-            record[1] = record[1].strip()
-        # convert date string to date
-            record_date = datetime.datetime.strptime(record[0], '%Y-%m-%d').date()
-        # if they studied today, it will be added to our 'time_studying' counter
-            if record_date == datetime.date.today():
-                todays_time_studying = todays_time_studying + float(record[1])
-    
-    # print the current time worked today
-    # TODO: Refactor to follow DRY
-    if todays_time_studying > 60 and todays_time_studying < 3600:
-        print(f'You\'ve studied for {todays_time_studying/60:.2f} minutes(s) today')
-    elif todays_time_studying > 3600:
-        print(f'You\'ve studied for {todays_time_studying/3600:.2f} hours(s) today')
-    else:
-        print(f'You\'ve studied for {todays_time_studying:.2f} second(s) today')
+import fileoperations
 
 # show the user how much time they've studied today
-print_todays_practice_time()
+fileoperations.print_todays_practice_time()
+
 # start the timer and the loop
 start = time.time()
 while True:
@@ -43,8 +15,10 @@ while True:
     if val == 'end':
         # end the timer
         break
+
 # calculate the time the program has run
 runtime = time.time() - start
+
 # print the time the program ran
 if runtime > 60:
     print(f'You spent {runtime/60:.2f} minute(s)')
@@ -55,6 +29,4 @@ else:
 # Print the date
 print(f'Today\'s date is {datetime.date.today()}')
 
-# log the user's study time to the timer_data.txt
-with open('timer_data.txt', 'a') as f:
-    f.write(f'{datetime.date.today()},{runtime:.2f}\n')
+fileoperations.write_new_record(runtime)
---
+++ fileoperations.py
@@ -0,0 +1,39 @@
+import datetime
+
+_filename = 'timer_data.txt'
+""" Display how much studying has been completed today """
+def print_todays_practice_time():
+    todays_time_studying = 0
+    
+    # read data from file -- if it doesn't exist then create it
+    with open(f'{_filename}', 'a+') as f:
+    # parse through the data and read in the records only for today
+        lines = f.readlines()
+    # go through each line, split it into an array, and look for today's date
+        for line in lines:
+            record = line.split(',')
+        # remove the newline character from the time
+            record[1] = record[1].strip()
+        # convert date string to date
+            record_date = datetime.datetime.strptime(record[0], '%Y-%m-%d').date()
+        # if they studied today, it will be added to our 'time_studying' counter
+            if record_date == datetime.date.today():
+                todays_time_studying = todays_time_studying + float(record[1])
+    
+    # print the current time worked today
+    # TODO: Refactor to follow DRY
+    if todays_time_studying > 60 and todays_time_studying < 3600:
+        print(f'You\'ve studied for {todays_time_studying/60:.2f} minutes(s) today')
+    elif todays_time_studying > 3600:
+        print(f'You\'ve studied for {todays_time_studying/3600:.2f} hours(s) today')
+    else:
+        print(f'You\'ve studied for {todays_time_studying:.2f} second(s) today')
+
+""" Write new record to file
+
+    Input Arguments: tunetime
+ """
+def write_new_record(runtime):
+    # log the user's study time to the timer_data.txt
+    with open(f'{_filename}', 'a') as f:
+        f.write(f'{datetime.date.today()},{runtime:.2f}\n')
