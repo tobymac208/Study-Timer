f02c6e1836c16118c8dcbc51624bc28157c9861a
nik <niklas_fernandez@comcast.net>
2021-08-20 07:42:38 -0500

    Adds the ability for a user to quickly check their time for the day.

---
+++ check_time.py
@@ -0,0 +1,6 @@
+from file_operations import time_data_operations
+
+
+if __name__ == "__main__":
+    # show the user how much time they've studied today
+    time_data_operations.print_todays_practice_progress()
